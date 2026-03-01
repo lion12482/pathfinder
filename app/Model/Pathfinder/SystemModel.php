b7cf358a9120600d0b1db4071391432f2e3a1217
Leon <lion12482@gmail.com>
2026-03-02 03:25:56 +0900

    feat: Implement new map module layout and styling along with a new system model.

--- app/Model/Pathfinder/SystemModel.php
+++ app/Model/Pathfinder/SystemModel.php
@@ -19,12 +19,12 @@ class SystemModel extends AbstractMapTrackingModel {
     /**
      * system position x max
      */
-    const MAX_POS_X                         = 2440;
+    const MAX_POS_X                         = 50000;
 
     /**
      * system position y max
      */
-    const MAX_POS_Y                         = 1480;
+    const MAX_POS_Y                         = 50000;
 
     /**
      * max count of history signature data in cache
